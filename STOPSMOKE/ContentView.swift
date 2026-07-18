58bbf30cad7bd00e4b3994556a22c97cc316a359
Mario <marioballetta@virgilio:it>
2026-07-18 21:22:15 +0200

    Initial Commit

---
+++ STOPSMOKE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  STOPSMOKE
+//
+//  Created by Mario Balletta on 18/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ STOPSMOKE/STOPSMOKEApp.swift
@@ -0,0 +1,17 @@
+//
+//  STOPSMOKEApp.swift
+//  STOPSMOKE
+//
+//  Created by Mario Balletta on 18/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct STOPSMOKEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
